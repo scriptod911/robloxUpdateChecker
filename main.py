68a0f0468579bb0e832bbd090b7f76e7ef29f798
scriptod911 <70215102+scriptod911@users.noreply.github.com>
2021-07-20 23:16:35 +0200

    some notes ig

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import requests
 import time
 
+
 def writeVersionToFile(version):
     f = open("versions.txt", "w")
+    f.truncate(0)
     f.write(version)
 
+
 def readVersionsFile():
-    f = open('versions.txt', 'r')
+    f = open("versions.txt", "r")
     return f.read()
 
 
-url = 'http://setup.roblox.com/version'
+url = "http://setup.roblox.com/version"
 res = requests.get(url)
 
 
 while res.status_code == 200:
     if res.text == readVersionsFile():
-        print("No updates")
+        print(
+            f"""
+No updates
+        
+The current update is: {res.text}
+"""
+        )
         time.sleep(60)
-    
+
     else:
         print("Update found! update: {0}".format(res.text))
         writeVersionToFile(res.text)
